c8840917a6ddc0cebe4a4ebe10a91232d6bc1913
sz <1911392294@qq.com>
2023-01-29 14:17:40 +0800

    [feat]格式化代码

--- src/extension.ts
+++ src/extension.ts
@@ -1,160 +1,194 @@
-const vscode = require('vscode');
+const vscode = require("vscode");
 const fs = require("fs");
 
-let fileData = ""; //读取文件后的字符数据
-let datas = []; //通过lineBreak分割后的字符数组
-let curPage = 0; //当前datas索引（分页记录）
-const tipTxt = "【阅读进度，勿删！Reading progress, do not delete!】"; //文件第一行存储阅读进度文字
+
+let fileData = ""; // 读取文件后的字符数据
+let datas:string[] = []; // 通过lineBreak分割后的字符数组
+let curPage = 0; // 当前datas索引（分页记录）
+const tipTxt = "【阅读进度，勿删！Reading progress, do not delete!】"; // 文件第一行存储阅读进度文字
 let currentText = "";
 
-const lineBreak = "\r\n"; //分页使用的分割字符
-let filePath = ""; //文件路径
-let replaceMark = "/*$*/"; //替换标记
+const lineBreak = "\r\n"; // 分页使用的分割字符
+let filePath = ""; // 文件路径
+// let replaceMark = "/*$*/"; // 替换标记
 
 
+// eslint-disable-next-line no-undef
 function activate(context: vscode.ExtensionContext) {
-	//变更当前激活文件时触发（用于记录阅读进度）
-	vscode.window.onDidChangeActiveTextEditor(() => {
-		saveReadProgress();
-	});
+  // 变更当前激活文件时触发（用于记录阅读进度）
+  vscode.window.onDidChangeActiveTextEditor(() => {
+    saveReadProgress();
+  });
 
-	//读取文件并插入注释
-	let disposable = vscode.commands.registerCommand('constReadNovel.constReadNovelStart', function () {
-		fileData = "";
-		datas = [];
-		curPage = 0;
-		// 没有设置文件地址
-		if (!vscode.workspace.getConfiguration().get('constReadNovel.filePath')) {
-			vscode.window.showErrorMessage('找不到此文件，请到设置里添加文件地址');
-			return;
-		}
-		filePath = vscode.workspace.getConfiguration().get('constReadNovel.filePath').replace(/(^\s*)|(\s*$)/g, "");
+  // 读取文件并插入注释
+  const disposable = vscode.commands.registerCommand("constReadNovel.constReadNovelStart", function () {
+    fileData = "";
+    datas = [];
+    curPage = 0;
 
-		replaceMark = vscode.workspace.getConfiguration().get('constReadNovel.replaceMark');
+    // 没有设置文件地址
+    if (!vscode.workspace.getConfiguration().get("constReadNovel.filePath")) {
+      vscode.window.showErrorMessage("找不到此文件，请到设置里添加文件地址");
 
-		//文件存在检测
-		if (filePath === "") {
-			vscode.window.showErrorMessage('文件不能存在，请到设置里添加文件地址');
-			return;
-		};
+      return;
+    }
 
-		//文件类型检测
-		if (getFileType(filePath) !== "txt") {
-			vscode.window.showErrorMessage('文件类型不正确，只支持utf-8编码的txt文件');
-			return;
-		};
+    filePath = vscode.workspace.getConfiguration().get("constReadNovel.filePath").replace(/(^\s*)|(\s*$)/g, "");
 
-		//打开文件
-		try {
-			fileData = fs.readFileSync(filePath, 'utf-8');
-		} catch (error) {
-			vscode.window.showErrorMessage(error.toString());
-			return;
-		}
+    // const replaceMark = vscode.workspace.getConfiguration().get("constReadNovel.replaceMark");
 
-		curPage = getSavePage(fileData);
-		datas = fileData.split(lineBreak);
-		console.log("过滤前段落长度：", datas.length);
-		datas = datas.filter(str => isEmptyLine(str));
-		console.log("过滤后段落长度：", datas.length);
-		currentText = datas[curPage];
-	});
+    // 文件存在检测
+    if (filePath === "") {
+      vscode.window.showErrorMessage("文件不能存在，请到设置里添加文件地址");
 
-	//下一页
-	let nextPage = vscode.commands.registerCommand('constReadNovel.ReadNovel_nextPage', function () {
-		currentText = datas[++curPage];
-	});
+      return;
+    }
 
-	//上一页
-	let prevPage = vscode.commands.registerCommand('constReadNovel.ReadNovel_prevPage', function () {
-		currentText = datas[--curPage];
-	});
+    // 文件类型检测
+    if (getFileType(filePath) !== "txt") {
+      vscode.window.showErrorMessage("文件类型不正确，只支持utf-8编码的txt文件");
 
-	//清空注释（老板键）
-	let clear = vscode.commands.registerCommand('constReadNovel.ReadNovel_clear', function () {
-		currentText = "CONST: The value of a constant can't be changed through reassignment";
-	});
+      return;
+    }
+
+    // 打开文件
+    try {
+      fileData = fs.readFileSync(filePath, "utf-8");
+    } catch (error) {
+      vscode.window.showErrorMessage(error.toString());
+
+      return;
+    }
+
+    curPage = getSavePage(fileData);
+    datas = fileData.split(lineBreak);
+    console.log("过滤前段落长度：", datas.length);
+    datas = datas.filter(str => isEmptyLine(str));
+    console.log("过滤后段落长度：", datas.length);
+    currentText = datas[curPage];
+  });
+
+  // 下一页
+  const nextPage = vscode.commands.registerCommand("constReadNovel.ReadNovel_nextPage", function () {
+    currentText = datas[++curPage];
+  });
+
+  // 上一页
+  const prevPage = vscode.commands.registerCommand("constReadNovel.ReadNovel_prevPage", function () {
+    currentText = datas[--curPage];
+  });
+
+  // 清空注释（老板键）
+  const clear = vscode.commands.registerCommand("constReadNovel.ReadNovel_clear", function () {
+    currentText = "CONST: The value of a constant canmott be changed through reassignment";
+  });
 
 
-	vscode.languages.registerHoverProvider("*", {
-		provideHover: async (document, position) => {
-			const word = document.getText(document.getWordRangeAtPosition(position));
-			if (word === "const") {
-				return new vscode.Hover(currentText);
-			}
-		}
-	});
+  vscode.languages.registerHoverProvider("*", {
+    provideHover: async (document, position) => {
+      const word = document.getText(document.getWordRangeAtPosition(position));
 
-	context.subscriptions.push(disposable);
-	context.subscriptions.push(nextPage);
-	context.subscriptions.push(prevPage);
-	context.subscriptions.push(clear);
+      if (word === "const") {
+        return new vscode.Hover(currentText);
+      }
+    }
+  });
+
+  context.subscriptions.push(disposable);
+  context.subscriptions.push(nextPage);
+  context.subscriptions.push(prevPage);
+  context.subscriptions.push(clear);
 }
 
 
-//获取文件类型
+// 获取文件类型
 function getFileType(filePath) {
-	var startIndex = filePath.lastIndexOf(".");
-	if (startIndex !== -1) {
-		return filePath.substring(startIndex + 1, filePath.length).toLowerCase();
-	} else {
-		return "";
-	}
+  const startIndex = filePath.lastIndexOf(".");
+
+  if (startIndex !== -1) {
+    return filePath.substring(startIndex + 1, filePath.length).toLowerCase();
+  }
+
+  return "";
 }
 
-//存储阅读进度
+// 存储阅读进度
 function saveReadProgress() {
-	if (!isLegal()) { return; };
-	let datas = fileData.split("\r\n");
-	if (datas.length === 0) { return; };
-	//如果没有记录进度则添加进度
-	if (datas[0].indexOf(tipTxt) === -1) {
-		let newData = curPage + tipTxt + "\r\n" + fileData;
-		writeFile(newData);
-	} else { //如果有进度则变更进度
-		if (curPage === 0) { return; };
-		let savePage = Number(datas[0].split(tipTxt)[0]);
-		if (curPage === savePage) { return; };
-		let oldLine = savePage + tipTxt + "\r\n";
-		let newLine = curPage + tipTxt + "\r\n";
-		let newData = fileData.replace(oldLine, newLine);
-		writeFile(newData);
-	}
+  if (!isLegal()) { return; }
+
+  ;
+  const datas = fileData.split("\r\n");
+
+  if (datas.length === 0) {
+    return;
+  }
+
+  // 如果没有记录进度则添加进度
+  if (datas[0].indexOf(tipTxt) === -1) {
+    const newData = curPage + tipTxt + "\r\n" + fileData;
+
+    writeFile(newData);
+  } else { // 如果有进度则变更进度
+    if (curPage === 0) { return; }
+
+    ;
+    const savePage = Number(datas[0].split(tipTxt)[0]);
+
+    if (curPage === savePage) {
+      return;
+    }
+
+    const oldLine = savePage + tipTxt + "\r\n";
+    const newLine = curPage + tipTxt + "\r\n";
+    const newData = fileData.replace(oldLine, newLine);
+
+    writeFile(newData);
+  }
 }
 
-//检测合法性
+// 检测合法性
 function isLegal() {
-	if (datas.length === 0 || curPage < 0 || curPage > datas.length) { return false; };
-	return true;
+  if (datas.length === 0 || curPage < 0 || curPage > datas.length) {
+    return false;
+  }
+
+  return true;
 }
 
-//写入文件
+// 写入文件
 function writeFile(data) {
-	fs.writeFile(filePath, data, { encoding: 'utf8' }, function (err) {
-		if (err) { return console.error(err); };
-	});
+  fs.writeFile(filePath, data, { encoding: "utf8" }, function (err) {
+    if (err) { return console.error(err); }
+
+    ;
+  });
 }
 
-//获取阅读进度
+// 获取阅读进度
 function getSavePage(fileData) {
-	const datas = fileData.split("\r\n");
-	if (datas[0].indexOf(tipTxt) === -1) { return curPage; };
-	let savePage = Number(datas[0].split(tipTxt)[0]);
-	return savePage;
+  const datas = fileData.split("\r\n");
+
+  if (datas[0].indexOf(tipTxt) === -1) {
+    return curPage;
+  }
+
+  const savePage = Number(datas[0].split(tipTxt)[0]);
+
+  return savePage;
 }
 
-//检测是否空行
+// 检测是否空行
 function isEmptyLine(str) {
-	return str.replace(/[ ]|[\r\n]/g, "") !== "";
+  return str.replace(/[ ]|[\r\n]/g, "") !== "";
 }
 
 
 // 销毁时保存阅读进度
 function deactivate() {
-	saveReadProgress();
+  saveReadProgress();
 }
 
 module.exports = {
-	activate,
-	deactivate
+  activate,
+  deactivate
 };
